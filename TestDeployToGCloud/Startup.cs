2cdd897271df62628fc1d9f24a5d6313a19097ba
fatah <yudafatah@gmail.com>
2021-03-20 12:50:11 +0700

    c1

--- TestDeployToGCloud/Startup.cs
+++ TestDeployToGCloud/Startup.cs
@@ -22,6 +22,7 @@ namespace TestDeployToGCloud
 
         public IConfiguration Configuration { get; }
 
+		// sss
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
